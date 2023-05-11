6fe425710e71b936c0918effb191ef7678abda41
Kyle Kryst <119367684+KyleKryst@users.noreply.github.com>
2023-05-11 14:05:54 -0700

    Created data.js
    Added my data file for my portfolio

---
+++ src/data.js
@@ -0,0 +1,51 @@
+import CodingQuiz from './assets/images/Coding-Quiz-Challenge.png';
+import Giffy from './assets/images/In-A-GIFFY.png';
+import PasswordGenerator from './assets/images/Passworde-Generator.png';
+import TechTurbo from './assets/images/Tech-Turbo-Blogsite.png';
+import WeatherApp from './assets/images/Whether-The-Weather.png';
+import WorkScheduler from './assets/images/Work-Day-Scheduler.png';
+
+export default [
+    {
+        "id": 1,
+        "name": "In A GIFFY",
+        "image": Giffy,
+        "github": "https://github.com/nKingchaos2284/bananas-pajamas",
+        "deploy": "https://in-a-gif.herokuapp.com/"
+    },
+    {
+        "id": 2,
+        "name": "Whether The Weather",
+        "image": WeatherApp,
+        "github": "https://github.com/KyleKryst/Whether-The-Weather",
+        "deploy": "https://kylekryst.github.io/Whether-The-Weather/"
+    },
+    {
+        "id": 3,
+        "name": "Tech Turbo Blogsite",
+        "image": TechTurbo,
+        "github": "https://github.com/KyleKryst/tech-turbo-blogsite",
+        "deploy": "https://tech-turbo-blogsite.herokuapp.com/"
+    },
+    {
+        "id": 4,
+        "name": "Password Generator",
+        "image": PasswordGenerator,
+        "github": "https://github.com/KyleKryst/Password-Generator",
+        "deploy": "https://kylekryst.github.io/Password-Generator/"
+    },
+    {
+        "id": 5,
+        "name": "Work Day Scheduler",
+        "image": WorkScheduler,
+        "github": "https://github.com/KyleKryst/Work-Day-Scheduler",
+        "deploy": "https://kylekryst.github.io/Work-Day-Scheduler/"
+    },
+    {
+        "id": 6,
+        "name": "Coding Quiz Challenge",
+        "image": CodingQuiz,
+        "github": "https://github.com/KyleKryst/Coding-Quiz-Challenge",
+        "deploy": "https://kylekryst.github.io/Coding-Quiz-Challenge/"
+    },
+]
